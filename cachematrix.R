cce42747e5abc39e8ecd3b093e6e9aa7a64df8f6
AndreaLauricella <64427209+AndreaLauricella@users.noreply.github.com>
2020-11-06 11:50:30 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## The first function, makeVector creates a special "matrix", which is really a list containing a function to
+## The first function, makeCacheMatrix creates a special "matrix", which is really a list containing a function to
 
     ## set the value of the vector
     ## get the value of the vector
